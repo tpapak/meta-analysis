17c4c9d59faaae65789f40cd4e9b9b0fa9387c9d
Thodoris Papakonstantinou <mail@tpapak.com>
2018-08-03 19:01:23 +0200

    meanDifference

--- src/Data/Meta.hs
+++ src/Data/Meta.hs
@@ -4,7 +4,8 @@
 
 module Data.Meta
   ( TreatmentId (..)
-  , Study
+  , Study (..)
+  , meanDifference
   ) where
 
 import           Control.Applicative
@@ -12,6 +13,7 @@ import qualified Data.Map.Strict     as Map
 import           Data.Maybe
 import           GHC.Generics
 import qualified Data.Csv             as C
+import Data.Either
 
 data TreatmentId = IntId Int
                  | StringId String
@@ -43,3 +45,32 @@ data Study =
 instance C.FromRecord Study
 instance C.FromNamedRecord Study
 instance C.ToNamedRecord Study
+
+data EffectSize =
+  EffectSize { effect :: !Double
+             -- |Variance of the mean (value)
+             , variance :: !Double
+             }
+  deriving (Generic,Read,Ord,Eq,Show)
+
+standardError :: EffectSize -> Double
+standardError es = sqrt $ variance es
+
+meanDifference :: Study -> Either String EffectSize
+meanDifference (BinaryStudy _ _ _ _ _) = 
+ Left "Binary outcome not continuous"
+meanDifference s = 
+  let ef = x1 - x2
+      var = sd1^2 / n1 + sd2^2 / n2
+   in Right $ EffectSize { effect = ef
+                    -- |Not assuming σ1 = σ2
+                         , variance = var
+                         }
+ where x1 = meanA s
+       sd1 = sdA s
+       n1 = fromIntegral $ nA s
+       x2 = meanB s
+       sd2 = sdB s
+       n2 = fromIntegral $ nB s
+
+
--- test/Test/Meta.hs
+++ test/Test/Meta.hs
@@ -7,6 +7,7 @@ import qualified Data.ByteString.Lazy             as B
 import qualified Data.Map.Strict                  as Map
 import qualified Data.Vector as V
 import qualified Data.Csv             as C
+import Data.Either
 
 import           TestHS
 
@@ -20,6 +21,7 @@ import           Data.Meta
 ioTests :: [IO Test]
 ioTests = [ test1
           , test2
+          , test3
           ]
 
 test1 :: IO Test
@@ -48,4 +50,19 @@ test2 = do
     Right (_, studies) -> do
       putStrLn "Studies file"
       putStrLn $ show studies
+      return $ testPassed name $ "passed!"
+
+test3 :: IO Test
+test3 = do
+  let name = "mean differences"
+  let studiesFile = "test/continuous.csv"
+  csvData <- B.readFile studiesFile
+  let estudies = C.decodeByName csvData
+               :: Either String (C.Header, V.Vector Study)
+  case estudies of
+    Left err -> return $ testFailed name $ ("error parsing csv",err)
+    Right (_, studies) -> do
+      let meandiffs = V.toList $ V.map meanDifference studies
+      putStrLn "mean differences of continuous"
+      putStrLn $ show $ rights meandiffs
       return $ testPassed name $ "passed!"
